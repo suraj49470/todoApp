221366089fbd2de39c3073b08cbc079d94c62bf7
suraj singh <suraj.singh@realtyredefined.com>
2017-07-13 16:45:43 +0530

    redux dev tool

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,10 @@
-import { createStore } from 'redux';
+import { createStore , applyMiddleware , compose} from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
 
 
 export default function configureStore(){
-	return createStore(rootReducer);
+	return createStore(rootReducer,compose(applyMiddleware(thunk),window.devToolsExtension ? window.devToolsExtension() : f => f));
 }
 
